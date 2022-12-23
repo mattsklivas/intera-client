97a29078239fd2fd1a8f243395cd6246e219d4b3
mattsklivas <msklivas@live.com>
2022-12-22 23:22:54 -0500

    nextjs code cleanup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: { API_URL: process.env.API_URL || 'http://localhost:3000/' },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Intera</title>
+        <meta
+          name="description"
+          content="Intera - An ASL/Speech to Text Faciliator \& Learning Platform"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
